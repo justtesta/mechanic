fdd5419a9b11530df5ae1b14ec3342887c436639
justtesta <36281733@qq.com>
2017-07-28 00:05:54 +0800

    dad

--- app/models/concerns/mechanic/importer.rb
+++ app/models/concerns/mechanic/importer.rb
@@ -32,7 +32,7 @@ class Mechanic < ApplicationRecord
           @error_messages << "未知区县：“#{@district}”"
           end
         end
-        @error_messages << "未知区县2：“#{ row[7].value}”"
+        @error_messages << "未知区县2：“#{ row[6].value}”"
           @address = row[3].value
           @nickname = row[4].value
           @mobile = row[5].value
