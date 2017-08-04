85a955edb3d315620a1984499fa8a09ea187c743
justtesta <36281733@qq.com>
2017-08-04 20:39:47 +0800

    d1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170731124944) do
+ActiveRecord::Schema.define(version: 20170804123558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -385,6 +385,14 @@ ActiveRecord::Schema.define(version: 20170731124944) do
     t.datetime "paid_at",    precision: 6
     t.index ["state_cd"], name: "index_withdrawals_on_state_cd", using: :btree
     t.index ["user_id"], name: "index_withdrawals_on_user_id", using: :btree
+  end
+
+  create_table "works", force: :cascade do |t|
+    t.integer  "mechanic_id"
+    t.integer  "skill_id"
+    t.integer  "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
