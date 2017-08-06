fa4d1f135c8fd27289968adfe0092a9009492bed
justtesta <36281733@qq.com>
2017-08-06 15:57:35 +0800

    e1

--- app/controllers/admin/mechanics_controller.rb
+++ app/controllers/admin/mechanics_controller.rb
@@ -53,7 +53,7 @@ end
     }
     @mechanic_params=mechanic_params
     @mechanic_params[:mechanic_attributes].delete :skills
-    @mechanic_params[:mechanic_attributes].[:skills]=works
+    @mechanic_params[:mechanic_attributes][:skills]=works
     if @mechanic.update_attributes(@mechanic_params)
       
       @mechanic.mechanic.works.replace(works)
