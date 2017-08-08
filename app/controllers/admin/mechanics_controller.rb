b28e98a7847da05726147e4b19bbf84852bc3aeb
justtesta <36281733@qq.com>
2017-08-09 00:42:16 +0800

    f

--- app/controllers/admin/mechanics_controller.rb
+++ app/controllers/admin/mechanics_controller.rb
@@ -62,11 +62,11 @@ class Admin::MechanicsController < Admin::ApplicationController
     }
     @mechanic_params=mechanic_params
     @mechanic_params[:mechanic_attributes].delete :skills
-    
+    @mechanic_params[:mechanic_attributes][:works]=[]
     if @mechanic.update_attributes(@mechanic_params)
-      @mechanic.mechanic.works.clear
+      #@mechanic.mechanic.works.clear
       @mechanic.mechanic.works.push(works)
-      #redirect_to admin_mechanics_path
+      redirect_to admin_mechanics_path
     else
       render :new
     end
