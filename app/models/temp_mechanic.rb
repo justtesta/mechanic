312238407044c197b2df5bda5a4548d5eea6ec6d
justtesta <36281733@qq.com>
2017-08-14 19:00:42 +0800

    f

--- app/models/temp_mechanic.rb
+++ app/models/temp_mechanic.rb
@@ -1,2 +1,21 @@
 class TempMechanic < ApplicationRecord
+  def description_hash
+    s = "M30L30Y10TW20TN90"
+    s = description
+    s=s.sub('NM',"")
+    s=s.sub('NL',"")
+    s=s.sub('JLD',"|JZD:")
+    s=s.sub('JLS',"|JZS:")
+    s=s.sub('M',"|M:")
+    s=s.sub('L',"|L:")
+    s=s.sub('Y',"|Y:")
+    s=s.sub('DJ',"|DJ:")
+    s=s.sub('TW',"|TW:")
+    s=s.sub('TN',"|TN:")
+    s[0]=""
+    
+    array = Array.new
+    array = s.split(/\:|\|/)
+    hash = Hash[*array]
+  end
 end
