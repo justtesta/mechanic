405dc92d125a6419d7355d44346ee226f879f662
justtesta <36281733@qq.com>
2017-08-15 10:02:20 +0800

    f

--- app/models/work.rb
+++ app/models/work.rb
@@ -20,5 +20,51 @@ class Work < ApplicationRecord
      # end
     end 
   end 
+  def get_skill_id (key)
+    case key
+    when "M"
+        29
+    when "L"
+        28
+    when "Y"
+        39
+    when "DJ"
+        15
+    when "TW"
+        37
+    when "TN"
+        34
+    when "JLD"
+        42
+    when "JLS"
+        41
+    else
+        0
+    end
+  end
+  
+  def set_price_by_description
+    TempMechanic.all.each do |temp_mechanic|
+      byebug
+      temp_mechanic.description_hash.each do |key,value|
+      _skill_id=get_skill_id key
+      @work = Work.where("mechanic_id = ? AND skill_id = ?", temp_mechanic.mechanic_id, _skill_id).last
+        if(@work.nil)
+          @work=Work.new
+          @work.mechanic_id=temp_mechanic.mechanic_id
+          @work.skill_id=_skill_id
+          @work.price = value  unless  value.nil?
+          @work.save
+        else
+          if(@work.price.blank?)
+          @work.price = value  unless  value.nil?
+          @work.save
+          end
+       end
+     end
+     
+    end 
+  end 
+
   
 end
