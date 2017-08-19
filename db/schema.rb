28a9fa1ec6c5fc4de33524af152619e40fdacf63
justtesta <36281733@qq.com>
2017-08-19 16:59:57 +0800

    First commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170804123558) do
+ActiveRecord::Schema.define(version: 20170814074555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -319,6 +319,17 @@ ActiveRecord::Schema.define(version: 20170804123558) do
 
   create_table "skills", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "temp_mechanics", force: :cascade do |t|
+    t.integer  "mechanic_id"
+    t.string   "mechanicname"
+    t.string   "addr"
+    t.string   "tel"
+    t.integer  "lbs_id"
+    t.string   "description"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "user_groups", force: :cascade do |t|
