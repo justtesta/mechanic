562ace7487429b96ab1f4cf02f28a2d911c47407
justtesta <36281733@qq.com>
2017-08-19 09:06:13 +0800

    e

--- app/controllers/merchants/orders_controller.rb
+++ app/controllers/merchants/orders_controller.rb
@@ -49,6 +49,8 @@ class Merchants::OrdersController < Merchants::ApplicationController
     if mechanic_id = params[:order][:mechanic_id]
       remark = params[:order][:remark]
       @order.update_attribute(:remark, remark) if remark
+      procedure_price = params[:order][:procedure_price]
+      @order.update_attribute(:procedure_price, procedure_price) if procedure_price
       mechanic = Mechanic.find(mechanic_id)
       if @order.assigned?
         @order.repick! mechanic
