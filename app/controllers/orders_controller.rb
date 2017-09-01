b01dbfae68fa0fe021882b0c7c8df8ee930108a8
justtesta <36281733@qq.com>
2017-09-01 11:06:31 +0800

    e

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -100,7 +100,7 @@ class OrdersController < ApplicationController
 
   def finish   
     if params.key? "verification"
-      if(@order.Numbers.where("status = 0").count>0 )
+      if(@order.numbers.where("status = 0").count>0 )
       flash.now[:error] = "请先核销消费码，如遇异常情况无法核销，请点(异常)服务完工人工审核提交"
       render :show
       end 
