cf05310d8291108d6d67a5c7a5b0c134c470b2ee
justtesta <36281733@qq.com>
2017-09-02 16:50:16 +0800

    e

--- app/models/temp_mechanic.rb
+++ app/models/temp_mechanic.rb
@@ -1,3 +1,33 @@
 class TempMechanic < ApplicationRecord
-
+  def description_hash
+    s = "M30L30Y10TW20TN90"
+    s = description.strip
+    s=s.sub('NM',"")
+    s=s.sub('NL',"")
+    s=s.sub('JLD',"|JZD:")
+    s=s.sub('JLS',"|JZS:")
+    s=s.sub('M',"|M:")
+    s=s.sub('L',"|L:")
+    s=s.sub('Y',"|Y:")
+    s=s.sub('DJ',"|DJ:")
+    s=s.sub('TW',"|TW:")
+    s=s.sub('TN',"|TN:")
+    s=s.sub('QSCP',"|QSCP:")
+    s=s.sub('ZSCP',"|ZSCP:")
+    s=s.sub('H',"|H:")
+    s[0]=""
+    
+    array = Array.new
+    array = s.split(/\:|\|/)
+    if (array.size%2==0)
+    hash = Hash[*array]
+    else
+    print "id:", mechanic_id, "\n"
+    Rails.logger.info "error mechanic_id:##{mechanic_id} "
+    
+    end
+    update_attribute(:addr,"1")
+    
+    
+  end
 end
