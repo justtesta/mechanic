31fa23c0e43e4a747fe93773f72c401c8f1fc0ea
justtesta <36281733@qq.com>
2017-09-09 20:52:22 +0800

    e

--- app/models/concerns/mechanic/importer.rb
+++ app/models/concerns/mechanic/importer.rb
@@ -37,7 +37,7 @@ class Mechanic < ApplicationRecord
           @mobile = row[5].value
           @unique_id = row[6].value
           @description = row[7].value
-          byebug
+          
           if(!row[8].blank?)
             unless row[8].value==0
               @service_ids = row[8].value.to_s.split()
