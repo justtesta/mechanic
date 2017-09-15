937fb7883e30783675122d147cec6d6f83974a18
justtesta <36281733@qq.com>
2017-09-15 11:08:25 +0800

    e

--- db/migrate/20170915030533_add_emergency_to_orders.rb
+++ db/migrate/20170915030533_add_emergency_to_orders.rb
@@ -1,5 +1,5 @@
 class AddEmergencyToOrders < ActiveRecord::Migration[5.0]
   def change
-    add_column :orders, :emergency, :boolean
+    add_column :orders, :emergency, :boolean, default: false
   end
 end
