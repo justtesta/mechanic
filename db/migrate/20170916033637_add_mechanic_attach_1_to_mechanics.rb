2c1d380cc42c96b97523126649d3de8f86fc2158
justtesta <36281733@qq.com>
2017-09-16 14:47:03 +0800

    e

--- db/migrate/20170916033637_add_mechanic_attach_1_to_mechanics.rb
+++ db/migrate/20170916033637_add_mechanic_attach_1_to_mechanics.rb
@@ -4,5 +4,6 @@ class AddMechanicAttach1ToMechanics < ActiveRecord::Migration[5.0]
     add_column :mechanics, :mechanic_attach_2, :string
     add_column :mechanics, :mechanic_attach_3, :string
     add_column :mechanics, :mechanic_attach_4, :string
+    add_column :mechanics, :mechanic_attach_5, :string
   end
 end
