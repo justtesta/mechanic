f9349a0f376c1b9da282b041e5039cfc877c1472
justtesta <36281733@qq.com>
2017-09-16 22:48:30 +0800

    e

--- db/migrate/20170916144606_add_userupdate_to_mechanics.rb
+++ db/migrate/20170916144606_add_userupdate_to_mechanics.rb
@@ -1,5 +1,5 @@
 class AddUserupdateToMechanics < ActiveRecord::Migration[5.0]
   def change
-    add_column :mechanics, :userupdate, :boolean
+    add_column :mechanics, :userupdate, :boolean, default: false
   end
 end
