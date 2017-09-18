dc1e390b3052231d1170d2a6f4a7a3eb2bfb18f0
justtesta <36281733@qq.com>
2017-09-18 19:15:21 +0800

    e

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:mobile, :nickname, :gender, :address, :avatar,
         mechanic_attributes: [:_create, :id, :province_cd, :city_cd, :district_cd, :description,
-        skill_cds: [] ])
+        skill_cds: [], service_ids: [] ])
     end
 
     def update_photo_user_params
