92380f35613bb6704ecfbd9232eaaadb10c6e4e3
justtesta <36281733@qq.com>
2017-09-23 09:23:02 +0800

    e

--- app/controllers/admin/metrics_controller.rb
+++ app/controllers/admin/metrics_controller.rb
@@ -4,7 +4,7 @@ class Admin::MetricsController < Admin::ApplicationController
   def index
     @metrics = if params[:mechanic_id]
         @mechanic = Mechanic.where(user_id: params[:mechanic_id]).first!
-        @mechanic.metrics
+        Metrics.where(user_id: params[:mechanic_id])
       else
         Metrics.all
       end
