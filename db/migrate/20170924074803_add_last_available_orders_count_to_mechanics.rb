f3749b4e523a09c91552fbf06541fce2ed00c959
justtesta <36281733@qq.com>
2017-09-24 15:48:22 +0800

    f

---
+++ db/migrate/20170924074803_add_last_available_orders_count_to_mechanics.rb
@@ -0,0 +1,6 @@
+class AddLastAvailableOrdersCountToMechanics < ActiveRecord::Migration[5.0]
+  def change
+    add_column :mechanics, :last_available_orders_count, :integer
+    add_column :mechanics, :last_available_orders_count, :integer
+  end
+end
