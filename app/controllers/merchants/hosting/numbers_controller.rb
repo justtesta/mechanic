e1e7161732476ba1084ba0ae68c21a55d8020274
justtesta <36281733@qq.com>
2017-09-26 21:22:09 +0800

    e

--- app/controllers/merchants/hosting/numbers_controller.rb
+++ app/controllers/merchants/hosting/numbers_controller.rb
@@ -1,4 +1,4 @@
-class Merchants::Hosting::NumbersController < Admin::ApplicationController
+class Merchants::Hosting::NumbersController < Merchants::ApplicationController
 
   def index
     @state = if %w(unchecked_numbers checked_numbers).include? params[:state]
@@ -7,6 +7,9 @@ class Merchants::Hosting::NumbersController < Admin::ApplicationController
         :unchecked_numbers
       end
     @numbers = Number.send(@state)
+  end
+  def checked?
+    status && ["1", 1, true].include?(status)
   end
   
 end
