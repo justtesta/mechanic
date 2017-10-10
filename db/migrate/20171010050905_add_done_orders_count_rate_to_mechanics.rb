a55535932f478bce4cba1c20cf86e0ea63028514
justtesta <36281733@qq.com>
2017-10-10 13:18:25 +0800

    e

--- db/migrate/20171010050905_add_done_orders_count_rate_to_mechanics.rb
+++ db/migrate/20171010050905_add_done_orders_count_rate_to_mechanics.rb
@@ -1,5 +1,5 @@
 class AddDoneOrdersCountRateToMechanics < ActiveRecord::Migration[5.0]
   def change
-    add_column :mechanics, :done_orders_count_rate, :decimal, scale: 2
+    add_column :mechanics, :done_orders_count_rate, :decimal,  precision: 10,  scale: 2
   end
 end
