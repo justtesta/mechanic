56c49542d237c9f2d0e1ec63490d09bf1e15f03d
justtesta <36281733@qq.com>
2017-11-10 14:25:29 +0800

    e

--- app/models/withdrawal.rb
+++ app/models/withdrawal.rb
@@ -32,7 +32,7 @@ class Withdrawal < ApplicationRecord
 
   def message
     return false unless pending?
-    Weixin.send_refund_order_message(self)
+    Weixin.send_confirm_order_message(self)
   end
 
   def title
