d12290481aad16d7c059f98cb2502a342bc254cc
justtesta <36281733@qq.com>
2017-11-15 16:53:23 +0800

    e

--- app/models/partcheck.rb
+++ app/models/partcheck.rb
@@ -4,12 +4,10 @@ class Partcheck < ApplicationRecord
 	def set_default default_order
 		byebug
 		self.order_id=default_order.id
-		_check_scale=0
-		_check_scale=default_order.numbers.checked_numbers.count/default_order.numbers.count if default_order.numbers.count>0
-		self.quoted_price=default_order.quoted_price * _check_scale
-		self.quantity=default_order.quantity * _check_scale if default_order.quantity
-		self.mechanic_income=default_order.mechanic_income * _check_scale if default_order.mechanic_income
-		self.procedure_price=default_order.procedure_price * _check_scale	if default_order.procedure_price	
+		self.quoted_price=default_order.quoted_price * default_order.numbers.checked_numbers.count/default_order.numbers.count if default_order.numbers.count>0
+		self.quantity=default_order.quantity * default_order.numbers.checked_numbers.count/default_order.numbers.count if default_order.numbers.count>0 && default_order.quantity
+		self.mechanic_income=default_order.mechanic_income * default_order.numbers.checked_numbers.count/default_order.numbers.count if default_order.numbers.count>0 &&  default_order.mechanic_income
+		self.procedure_price=default_order.procedure_price * default_order.numbers.checked_numbers.count/default_order.numbers.count if default_order.numbers.count>0 &&  default_order.procedure_price	
 	end
 	
 	def profit
