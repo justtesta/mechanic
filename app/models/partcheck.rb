84033fba4965dc0fe7176e9b2cb005792cd529e8
justtesta <36281733@qq.com>
2017-11-15 16:22:25 +0800

    e

--- app/models/partcheck.rb
+++ app/models/partcheck.rb
@@ -2,13 +2,14 @@ class Partcheck < ApplicationRecord
 	belongs_to :order
 
 	def set_default default_order
+		byebug
 		self.order_id=default_order.id
 		_check_scale=0
 		_check_scale=default_order.numbers.checked_numbers.count/default_order.numbers.count if default_order.numbers.count>0
-		self.quoted_price=default_order.quoted_price*_check_scale
-		self.quantity=default_order.quantity*_check_scale
-		self.mechanic_income=default_order.mechanic_income*_check_scale
-		self.procedure_price=default_order.procedure_price*_check_scale		
+		self.quoted_price=default_order.quoted_price * _check_scale
+		self.quantity=default_order.quantity * _check_scale
+		self.mechanic_income=default_order.mechanic_income * _check_scale
+		self.procedure_price=default_order.procedure_price * _check_scale		
 	end
 	
 	def profit
