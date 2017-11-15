bb14413b1554ebb309538d509db6c46b9a6f0277
justtesta <36281733@qq.com>
2017-11-15 12:49:44 +0800

    e

--- app/controllers/merchants/hosting/partchecks_controller.rb
+++ app/controllers/merchants/hosting/partchecks_controller.rb
@@ -1,7 +1,7 @@
 class Merchants::Hosting::PartchecksController < Merchants::ApplicationController
   before_action :redirect_user_dispatcher
   def index
-    
+    @order = Order.hostings.find(params[:order_id])
     @partchecks = @order.partchecks
   end
 
