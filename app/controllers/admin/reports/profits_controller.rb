4b4227aa44ba8b1cc2e0c21ce0b7c1dc553116a1
justtesta <36281733@qq.com>
2017-11-16 10:26:15 +0800

    e

--- app/controllers/admin/reports/profits_controller.rb
+++ app/controllers/admin/reports/profits_controller.rb
@@ -1,6 +1,6 @@
 class Admin::Reports::ProfitsController < Admin::ApplicationController
 	before_action :redirect_user
     def index
-   		@orders=Order.settleds.where("orders.confirm_by>0").includes(:user, :mechanic_user).order(finish_working_at: :desc)   
+   		@orders=Order.settleds.where("orders.confirm_by>0").includes(:user, :mechanic_user).joins(:user).order(finish_working_at: :desc)   
     end
 end
