ea93c09fb92568650bb753f73d5abf63fb7cd7a4
justtesta <36281733@qq.com>
2017-11-19 23:15:26 +0800

    e

--- db/migrate/20171119133925_add_automatic_confirm_to_orders.rb
+++ db/migrate/20171119133925_add_automatic_confirm_to_orders.rb
@@ -1,5 +1,5 @@
 class AddAutomaticConfirmToOrders < ActiveRecord::Migration[5.0]
   def change
-    add_column :orders, :automatic_confirm, :boolean
+    add_column :orders, :automatic_confirm, :boolean, default: false
   end
 end
