4fef5afcb6e0486a41ed15932229cf6ce6dbe29f
justtesta <36281733@qq.com>
2017-11-20 10:31:37 +0800

    e

--- app/controllers/withdrawals_controller.rb
+++ app/controllers/withdrawals_controller.rb
@@ -63,13 +63,13 @@ class WithdrawalsController < ApplicationController
     end
 
     def current_weixin_openid!
-      if(session[:openid].present)
+      if(session[:openid].present?)
           @openid = session[:openid]
       else
           session[:openid] ="ddd"
       end
       if request.get? && weixin? && current_user 
-        if(session[:openid].present)
+        if(session[:openid].present?)
           @openid = session[:openid]
         else
           if params.key? "code"
