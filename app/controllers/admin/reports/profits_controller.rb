539c7a954c52c905cd06b0fad946fed97d2b333e
justtesta <36281733@qq.com>
2017-11-21 23:30:58 +0800

    e

--- app/controllers/admin/reports/profits_controller.rb
+++ app/controllers/admin/reports/profits_controller.rb
@@ -1,6 +1,6 @@
 class Admin::Reports::ProfitsController < Admin::ApplicationController
 	before_action :redirect_user
     def index
-   		@orders=Order.settleds.includes(:user, :mechanic_user).joins(:user).order(finish_working_at: :desc)   
+   		@orders=Order.settleds.where("finish_working_at>'2017-10-31 23:59'").includes(:user, :mechanic_user).joins(:user).order(finish_working_at: :desc)   
     end
 end
