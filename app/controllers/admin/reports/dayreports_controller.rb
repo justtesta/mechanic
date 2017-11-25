ac85a141a813148a0cd6c3ccfe8d40a2cfc0129f
justtesta <36281733@qq.com>
2017-11-25 20:24:18 +0800

    e

--- app/controllers/admin/reports/dayreports_controller.rb
+++ app/controllers/admin/reports/dayreports_controller.rb
@@ -1,6 +1,17 @@
 class Admin::Reports::DayreportsController < Admin::ApplicationController
 	before_action :redirect_user
     def index
+    	@end_date = if(params[:end_date].present?) 
+	      Date.parse(params[:end_date])
+	    else
+	      Date.today 
+	    end 
+	    if @end_date.present?
+	      @orders=Order.settleds.where(finish_working_at: (@end_date)..(@end_date+1.day))
+	    end
+
+	    @orders_profit=@orders.profit.sum()
+    	
     
     end
 end
