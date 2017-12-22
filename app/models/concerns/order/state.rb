23ca1211b5fbe4f42dfcfbc7985e9a2151f21b52
justtesta <36281733@qq.com>
2017-12-22 22:11:51 +0800

    e

--- app/models/concerns/order/state.rb
+++ app/models/concerns/order/state.rb
@@ -194,6 +194,14 @@ class Order < ApplicationRecord
       end
 
       def finish!
+        if(confirming) then
+          errors.add(:base, "该订单已提交审核，请耐心等待管理员审核")
+          return false
+        end
+        if(finished) then
+          errors.add(:base, "该订单已确认完工。")
+          return false
+        end
         unless(working?||paid?) then
           errors.add(:base, "订单状态不对，是否已经由客服确认完工")
           return false
