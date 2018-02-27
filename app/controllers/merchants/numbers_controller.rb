c0483074f919907d2f07a82996d4ff55bb2b6fff
justtesta <36281733@qq.com>
2018-02-27 19:17:33 +0800

    e

---
+++ app/controllers/merchants/numbers_controller.rb
@@ -0,0 +1,26 @@
+class Merchants::NumbersController < Merchants::ApplicationController
+  before_action :find_note
+
+  def index
+    @numbers = Number.first()
+  end
+
+  def update
+    if @note.update_attributes(note_params)
+      flash[:notice] = "成功更新记事本！"
+      redirect_to merchants_note_path
+    else
+      render :show
+    end
+  end
+
+  private
+
+    def find_note
+      @note = current_store.notes.first_or_create
+    end
+
+    def note_params
+      params.require(:note).permit(:content)
+    end
+end
