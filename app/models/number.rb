d323e4e9c739c640bbaa626e65a40dfb397f4d53
justtesta <36281733@qq.com>
2018-02-28 23:09:38 +0800

    e

--- app/models/number.rb
+++ app/models/number.rb
@@ -10,6 +10,12 @@ class Number < ApplicationRecord
   def right_number
     pwd_number_default[9,3]
   end
+  def left_number2
+    pwd_number_default[0,-3]
+  end
+  def right_number2
+    pwd_number_default.last(3)
+  end
   scope :unchecked_numbers, -> { where(status: 0) }
   scope :checked_numbers, -> { where(status: 1) }
 end
