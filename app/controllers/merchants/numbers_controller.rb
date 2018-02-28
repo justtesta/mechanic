c56c7db8c5fa72b19f9e0b909f525a27a93887c2
justtesta <36281733@qq.com>
2018-02-28 17:30:42 +0800

    e

--- app/controllers/merchants/numbers_controller.rb
+++ app/controllers/merchants/numbers_controller.rb
@@ -1,19 +1,18 @@
 class Merchants::NumbersController < Merchants::ApplicationController
+  before_action :find_order
   before_action :find_number , only: [ :edit, :update, :destroy ]
+  before_action :redirect_mechanic
 
 
   def index
-    @order = Order.find(params[:order_id])
     @numbers = @order.numbers
   end
 
   def new
-    @order = Order.find(params[:order_id])
     @number =@order.numbers.new
   end
 
   def create
-    @order = Order.find(params[:order_id])
     @number = @order.numbers.new(number_params)
     if @number.save
       redirect_to merchants_order_numbers_path
@@ -42,9 +41,16 @@ class Merchants::NumbersController < Merchants::ApplicationController
 
   private
 
+    def find_order
+        @order = order_klass.find(params[:order_id])
+    end
+
     def find_number
-      @order = Order.find(params[:order_id])
       @number =@order.numbers.find(params[:id])
+    end
+
+    def order_klass
+      Order.where(user_id: current_store, merchant_id: current_merchant)
     end
 
     def number_params
