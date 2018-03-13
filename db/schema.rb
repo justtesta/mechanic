710298672488d11f002ca3c613a0ae2f865a1261
justtesta <36281733@qq.com>
2018-03-13 11:37:48 +0800

    f

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180310003744) do
+ActiveRecord::Schema.define(version: 20180313031624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -211,6 +211,7 @@ ActiveRecord::Schema.define(version: 20180310003744) do
     t.string   "store_mobile"
     t.string   "store_address"
     t.string   "store_hotline"
+    t.integer  "jd_vender_id"
     t.index ["mobile"], name: "index_merchants_on_mobile", unique: true, using: :btree
     t.index ["role_cd"], name: "index_merchants_on_role_cd", using: :btree
     t.index ["user_id"], name: "index_merchants_on_user_id", using: :btree
